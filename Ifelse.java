a84d1d5795805fc6e471f6f9abdee6ee207f6039
Hiroid <77084690+ahmad0918@users.noreply.github.com>
2021-01-19 09:06:17 +0700

    Bikin codingan yang bener...!!!!
    typo mulu...!!

--- Ifelse.java
+++ Ifelse.java
@@ -5,7 +5,9 @@ public class Percabanganelse {
         int umur = scanner.nextInt();
         if (umur > 15) {
             System.out.println("Anda boleh masuk");
-        } else {
+        }else if (umur < 14) { 
+            System.out.println("Masuk Bayar Rp. 50.000");
+        }else {
             System.out.println(" Dilarang masuk ");
     }
   }
